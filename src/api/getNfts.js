65c4d4ba4e51e16c96f4b9d20d0e99477b9d0edb
unknown <ccamogli@yahoo.com>
2023-03-15 22:28:48 -0300

    hardcode key, will later delete it

--- src/api/getNfts.js
+++ src/api/getNfts.js
@@ -2,11 +2,13 @@ import { Alchemy, Network, NftFilters } from "alchemy-sdk";
 import { nanoid } from "nanoid";
 
 const config = {
-    apiKey: import.meta.env.VITE_ALCHEMY_API_KEY || process.env.ALCHEMY_API_KEY,
+    apiKey: 'NTG8XcVQ4vkySq23ZhQuqD64pR4ZS1Lt', // for production use import.meta.env.VITE_ALCHEMY_API_KEY,
     network: Network.ETH_MAINNET
 }
 
 const alchemy = new Alchemy(config);
+
+console.log(config);
 
 export async function getNfts(address, pageNumber="") {
     const nfts = await alchemy.nft.getNftsForOwner(address, {
